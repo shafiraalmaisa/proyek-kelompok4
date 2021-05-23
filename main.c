7796fef842b6073071864d174188499093e9a02a
birrulwalidain09 <84619047+birrulwalidain09@users.noreply.github.com>
2021-05-23 03:29:20 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 char pg1, pg2, pg3, pg4, bs5, bs6, bs7;
 
@@ -72,7 +74,50 @@ void BS() {
   printf("\n<<________________________________________________________________>>\n");
 }
 
-int main() {
+int main(int ArgumenAwal,char *argumen[]) {
+
+    if(ArgumenAwal !=3){
+        printf("Maaf anda gagal untuk login!\nAkses lebih lanjut ditolak");
+        printf("\nCara penggunaan : ./FileAplikasiProgramUtama username password");
+
+
+    }
+
+    char usernameInput[10],passwordInput[10];
+    strcpy(usernameInput,argumen[1]);
+    strcpy(passwordInput,argumen[2]);
+
+
+    FILE *fpr;
+
+    if((fpr = fopen("Database/login.bin","rb")) == NULL){
+        printf("gagal membuka FILE Database/login.bin");
+        return EXIT_FAILURE;
+    }
+
+char akun[25];
+fread(akun,sizeof(char),sizeof(akun)/sizeof(char),fpr);
+
+fclose(fpr);
+
+char *string[5];
+char username[25],password[25];
+int ctrl = 0;
+
+string [0] = strtok(akun,"/");
+while(string[ctrl++]  != NULL){
+    string[ctrl] = strtok(NULL,"/");
+}
+
+strcpy(username,string[0]);
+strcpy(password,string[1]);
+
+if( (strcmp(usernameInput,username)== 0) &&(strcmp(passwordInput,password)== 0)){
+    printf("Berhasil Login!\n");
+}
+else{
+     printf("Gagal login!\n");
+ }
 
   opening();
 
